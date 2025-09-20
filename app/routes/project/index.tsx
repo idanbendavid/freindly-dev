fe0e364c91ca1f0d5825f7e6f95b5e8b3aea797c
idanbendavid <idanbendavid10@hotmail.com>
2025-09-20 15:01:07 +0300

    react-router SSR loader

--- app/routes/project/index.tsx
+++ app/routes/project/index.tsx
@@ -1,9 +1,30 @@
-const ProjectsPage = () => {
+import axios from "axios";
+import type { Route } from "./+types/index";
+import type { Project } from "~/Types/types";
+import ProjectCard from "~/components/ProjectCard";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+    const response = await axios.get("http://localhost:8000/projects");
+    const data = response.data;
+
+    return { projects: data };
+}
+
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+
+    const { projects } = loaderData as { projects: Project[] };
+
     return (
         <>
             <h2 className="text-3xl text-white font-bold mb-8">
                 Projects
             </h2>
+            <div className="grid gap-6 sm:grid-cols-2">
+                {projects.map((project) => (
+                    <ProjectCard project={project} key={project.id} />
+                ))}
+            </div>
         </>
     );
 }
